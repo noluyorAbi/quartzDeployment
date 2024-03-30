1d8cc112cc6aab36165614e6efd74a36feb8cea4
Adatepe <adatepe.alperen.lmu@gmail.com>
2024-03-30 01:38:22 +0100

    Quartz sync: Mar 30, 2024, 1:38 AM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -33,7 +33,7 @@ const defaultOptions = {
       return -1
     }
   },
-  filterFn: (node) => node.name !== "tags",
+  filterFn: (node) => node.name !== "tags" && node.name !=="The-value-of-the-cloak-is-known-in-the-rain.", // "The-value-of-the-cloak-is-known-in-the-rain." will be excluded from the explorer view.
   order: ["filter", "map", "sort"],
 } satisfies Options
 
