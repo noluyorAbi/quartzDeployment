9d86340839fbf5a98c339be31410d10854b83014
Adatepe <adatepe.alperen.lmu@gmail.com>
2024-04-04 16:16:05 +0200

    Quartz sync: Apr 4, 2024, 4:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.myuninotes.com", // davor quartz.jzhao.xyz
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
