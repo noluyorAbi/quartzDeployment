90e4f9199b12fa75158a901dd4c23fc5bd64507a
noluyorAbi <121226886+noluyorAbi@users.noreply.github.com>
2024-04-11 02:47:59 +0200

    PageTitle.tsx undo
    Did not work imma sleep

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -4,10 +4,8 @@ import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  //const
-  let title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  if(title === "Studying made easy.") title = "ABC"
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
