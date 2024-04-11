e279017db512a9f99f6cc43479dde38817a69db7
noluyorAbi <121226886+noluyorAbi@users.noreply.github.com>
2024-04-11 02:42:22 +0200

    PageTitle.tsx test

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,6 +6,7 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  if(title === "Studying made easy.") title = "ABC"
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
