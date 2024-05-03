1ca6fa155e1fbf73516e76ee1cf7cb43257e6510
Adatepe <adatepe.alperen.lmu@gmail.com>
2024-05-03 11:02:53 +0200

    Quartz sync: May 3, 2024, 11:02 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,3 +1,4 @@
+// Import necessary dependencies and types
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
@@ -5,24 +6,38 @@ import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
+  paypalMeLink: string
 }
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const paypalMeLink = opts?.paypalMeLink ?? "https://paypal.me/YourPaypalMeLink"
+
     return (
-      <footer class={`${displayClass ?? ""}`}>
+      <footer className={`${displayClass ?? ""} ${style.footer}`}>
         <hr />
-        <a href={"/The-value-of-the-cloak-is-known-in-the-rain."}>The value of the cloak is known in the rain.</a>
         <p>
+          {/* This section is commented out but can be used for additional footer elements
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://github.com/noluyorAbi">noluyorAbi</a> © {year}
+          <a href="https://github.com/username">username</a> © {year}
           <br />
-          {/* <span>
-            Made with<a href="https://github.com/jackyzha0/quartz"> Quartz v{version}</a>
-          </span> */}
+          Made with <a href="https://github.com/jackyzha0/quartz">Quartz v{version}</a>
+          */}
         </p>
+        <p>
+          If this site has been helpful, consider supporting me with a donation:
+          <a href={paypalMeLink} target="_blank" rel="noopener noreferrer">
+            {" "}
+            Donate via PayPal
+          </a>
+        </p>
+        <a href="/The-value-of-the-cloak-is-known-in-the-rain.">
+          The value of the cloak is known in the rain.
+        </a>
+        <br />
+        <br />
         <a href="mailto:contact@myuninotes.com">contact@myuninotes.com</a>
       </footer>
     )
@@ -30,4 +45,4 @@ export default ((opts?: Options) => {
 
   Footer.css = style
   return Footer
-}) satisfies QuartzComponentConstructor
+}) as QuartzComponentConstructor
